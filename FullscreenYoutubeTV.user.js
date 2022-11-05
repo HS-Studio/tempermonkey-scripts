a241329db3ba2b93cfc84891105311eb7ff2ca8e
HS-Studio <91321551+HS-Studio@users.noreply.github.com>
2022-11-05 12:31:15 +0100

    Update FullscreenYoutubeTV.user.js

--- FullscreenYoutubeTV.user.js
+++ FullscreenYoutubeTV.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Fullscreen Youtube TV
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       SkyHacker
 // @match        https://www.youtube.com/tv
